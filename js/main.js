a0b973544efc9be55297b0f201aa4404b86414e0
Darrion Ramdin <darrionr007@gmail.com>
2018-10-26 06:55:03 -0700

    Finished: Footer + Portfolio Item Page

--- js/main.js
+++ js/main.js
@@ -17,31 +17,71 @@ $(function() {
     }
 
     // Parallax Feature
-    const rellax = new Rellax('.jumbotron-content');
+    parallax();
 
     // Navbar Scroll Background
     $(document).scroll(function() {
-        $('.navbar').toggleClass('navbar-scrolled', $(this).scrollTop() > $('.navbar').height());
+      navBackground()
     });
 
     // Smooth Scrolling
-    $('.navbar .nav-link, .scrollTo').on("click", function (e) {
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top - 50
-        }, 1000);
-        e.preventDefault();
-    });
+    smoothScroll();
 
     // Lightbox
-    $('.fancy-box').fancybox();
+    lightbox();
 
     // Portfolio Filter
-    const filterizd = $('.filtr-container').filterizr();
+
 
     // Portfolio Filter Active
+    portfolioFilters();
+});
+
+// Main Functions
+
+// Parallax Feature
+function parallax() {
+  if($('.rellax').length) {
+    const rellax = new Rellax('.jumbotron-content');
+  }
+}
+
+//  Lightbox
+function lightbox() {
+  if($('.fancy-box').length) {
+    $('.fancy-box').fancybox();
+  }
+}
+
+// Smooth Scrolling
+function smoothScroll() {
+  $('.navbar .nav-link, .scrollTo').on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top - 50
+      }, 1000);
+      e.preventDefault();
+  });
+}
+
+// Portfolio Filter
+function portfolioFilter() {
+  if($('.filtr-container').length) {
+    const filterizd = $('.filtr-container').filterizr();
+  }
+}
+
+// Portfolio filters
+function portfolioFilters() {
+  if($('.portfolio-filters').length) {
     $('.portfolio-filters button').on('click', function() {
         $('.portfolio-filters button').removeClass('active');
         $(this).addClass('active');
     })
-})
+  }
+}
+
+// Change Navbar background
+function navBackground() {
+  $('.navbar').toggleClass('navbar-scrolled', $(this).scrollTop() > $('.navbar').height());
+}
